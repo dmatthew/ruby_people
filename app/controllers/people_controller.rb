569b7542c835e3d811142e2ca66fdbe5a18d235c
Matt Davis <madavis111@outlook.com>
2017-07-19 16:04:43 -0700

    Implement remaining controller actions

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,56 @@
 class PeopleController < ApplicationController
-  def new
+  # Route: GET /
+  def index
+    @people = Person.all
   end
 
-  def create
-    render plain: params[:person].inspect
+  # Route: GET /people/:id
+  def show
+    @person = Person.find(params[:id])
   end
+
+  # Route: GET /people/new
+  def new
+    @person = Person.new
+  end
+
+  # Route: GET /people/:id/edit
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  # Route: POST /people
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to @person
+    else
+      render 'new'
+    end
+  end
+
+  # Route: PATCH/PUT /people/:id
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(person_params)
+      redirect_to @person
+    else
+      render 'edit'
+    end
+  end
+
+  # Route: DELETE /people/:id
+  def destroy
+    @person = Person.find(params[:id])
+    @person.destroy
+
+    redirect_to people_path
+  end
+
+  private
+    def person_params
+      params.require(:person).permit(:name, :age, :occupation)
+    end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,9 @@
 class Person
   include Mongoid::Document
+  include ActiveModel::Validations
   field :name, type: String
   field :age, type: Integer
   field :occupation, type: String
+
+  validates :name, presence: true
 end
