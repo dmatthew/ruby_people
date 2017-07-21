538c323c6c13c6874a0e4c0410428c9cb9ccaf24
Matt Davis <madavis111@outlook.com>
2017-07-21 12:37:48 -0700

    Delete dependent relation.

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,7 +5,7 @@ class Person
   field :age, type: Integer
   field :occupation, type: String
 
-  embeds_many :tasks, dependent: :destroy
+  embeds_many :tasks
 
   validates :name, presence: true
 end
